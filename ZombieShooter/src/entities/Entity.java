d8a123e236030b5f521c4ee6b466fced6b1e3e48
Dylan Socolobsky <dsocolobsky@gmail.com>
2014-01-28 12:40:55 -0300

    Add Entity class

---
+++ ZombieShooter/src/entities/Entity.java
@@ -0,0 +1,30 @@
+package entities;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Entity {
+	public Vector2 position;
+	public Vector2 size;
+	public Vector2 direction;
+	public float speed;
+	private Rectangle bounds;
+	
+	public Entity(float x, float y, float wd, float ht) {
+		position  = new Vector2(x, y);
+		size      = new Vector2(wd, ht);
+		direction = new Vector2(0, 0);
+		
+		speed = 0;
+		
+		bounds = new Rectangle(x, y, wd, ht);
+	}
+	
+	public void update() {
+		updateBounds();
+	}
+	
+	public void updateBounds() {
+		bounds.set(position.x, position.y, size.x, size.y);
+	}
+}
