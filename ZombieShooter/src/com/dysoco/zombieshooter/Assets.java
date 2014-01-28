5bfaa2f21591d2b10d2a5fd2a8c84a808a05e3a5
Dylan Socolobsky <dsocolobsky@gmail.com>
2014-01-28 13:32:27 -0300

    Add dummy player texture.

--- ZombieShooter/src/com/dysoco/zombieshooter/Assets.java
+++ ZombieShooter/src/com/dysoco/zombieshooter/Assets.java
@@ -1,9 +1,14 @@
 package com.dysoco.zombieshooter;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public class Assets {
 	
+	public static Texture playertxt;
+	
 	public static void load() {
-		
+		playertxt = new Texture(Gdx.files.internal("data/player.png"));
 	}
 	
 	public static void dispose() {
